93234db71d6d7690cac9a570e7a5360e05613e19
Vladyslav Vyshynskyi <crusotdeath@gmail.com>
2024-05-31 09:19:35 +0300

    AppConfig.java - fixes to be able to read enviroment variables BotService.java - handlers added

--- src/main/java/online/carsharing/config/AppConfig.java
+++ src/main/java/online/carsharing/config/AppConfig.java
@@ -1,24 +1,25 @@
 package online.carsharing.config;
 
+import lombok.RequiredArgsConstructor;
 import online.carsharing.service.impl.BotService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Configuration
 @ComponentScan("online.carsharing")
+@RequiredArgsConstructor
 public class AppConfig {
 
+    private final BotService botService;
+
     @Bean
-    public BotService registration() {
-        BotService bot = new BotService();
-        try {
-            new TelegramBotsApi(DefaultBotSession.class).registerBot(bot);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return bot;
+    public TelegramBotsApi telegramBotsApi() throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(botService);
+        return telegramBotsApi;
     }
 }
--- src/main/java/online/carsharing/service/impl/BotService.java
+++ src/main/java/online/carsharing/service/impl/BotService.java
@@ -1,9 +1,11 @@
 package online.carsharing.service.impl;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import online.carsharing.entity.User;
 import online.carsharing.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -17,51 +19,65 @@ public class BotService extends TelegramLongPollingBot {
     @Autowired
     private UserService userService;
 
-//    @Value("${bot.botname}")
-//    private String botName;
-//
-//    @Value("${bot.bottoken}")
-//    private String botToken;
+    @Value("${bot.botname}")
+    private String botName;
+
+    @Value("${bot.bottoken}")
+    private String botToken;
+
 
     @Override
     public String getBotUsername() {
-        return "testuser30052024_bot";
+        return this.botName;
     }
 
     @Override
     public String getBotToken() {
-        return "7180426233:AAHm-PmgYLcjG1yliXeVUlAHUerw0ziiMvA";
+        return this.botToken;
     }
 
     @Async
     @Override
     public void onUpdateReceived(Update update) {
-        Long chatId = update.getMessage().getChatId();
-        boolean hasText = update.hasMessage() && update.getMessage().hasText();
-        String command = update.getMessage().getText();
-        if (hasText && command.equals("/addMe")) {
-            if (!userService.existsByChatId(chatId)) {
-                User user = new User();
-                user.setChatId(chatId);
-                user.setSubscribe(false);
-                userService.save(user);
-                sendMessage(update, "Thats OK!");
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            Long chatId = update.getMessage().getChatId();
+            String command = update.getMessage().getText();
+
+            if (command.equals("/addMe")) {
+                handleAddMeCommand(chatId, update);
+            } else if (command.equals("/me")) {
+                handleMeCommand(chatId, update);
             } else {
-                sendMessage(update, "U are in table");
+                sendMessage(update, "Unknown command");
             }
-        } else if (hasText && command.equals("/me")) {
-            sendMessage(update, userService.inTable(chatId).toString());
         }
     }
 
-    public void sendMessage(Update update, String text) {
+    private void handleAddMeCommand(Long chatId, Update update) {
+        if (!userService.existsByChatId(chatId)) {
+            User user = new User();
+            user.setChatId(chatId);
+            user.setSubscribe(false);
+            userService.save(user);
+            sendMessage(update, "Thats OK!");
+        } else {
+            sendMessage(update, "You are already in the table");
+        }
+    }
+
+    private void handleMeCommand(Long chatId, Update update) {
         try {
-            execute(
-                    SendMessage.builder()
-                            .chatId(update.hasMessage() ? update.getMessage().getChatId()
-                                    : update.getCallbackQuery().getFrom().getId())
-                            .text(text)
-                            .build());
+            sendMessage(update, userService.inTable(chatId).toString());
+        } catch (Exception e) {
+            sendMessage(update, "Error fetching user info");
+            e.printStackTrace();
+        }
+    }
+
+    private void sendMessage(Update update, String text) {
+        Long chatId = update.hasMessage() ? update.getMessage().getChatId() : update.getCallbackQuery().getFrom().getId();
+        try {
+            execute(SendMessage.builder().chatId(chatId.toString()).text(text).build());
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
