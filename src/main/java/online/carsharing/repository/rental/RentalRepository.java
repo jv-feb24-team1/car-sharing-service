e4c01cab9d255699f9add48fc63beb8132dd014b
Vladyslav Vyshynskyi <crusotdeath@gmail.com>
2024-06-02 17:35:17 +0300

    Merge branch telegram-implementation into telegram-scheduling-implementation Refactor - RentalRepository - Removed @EntityGraph Left Join Fetch used instead

--- src/main/java/online/carsharing/repository/rental/RentalRepository.java
+++ src/main/java/online/carsharing/repository/rental/RentalRepository.java
@@ -3,15 +3,15 @@ package online.carsharing.repository.rental;
 import java.time.LocalDate;
 import java.util.List;
 import online.carsharing.entity.Rental;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface RentalRepository extends JpaRepository<Rental, Long> {
     @Query("SELECT r FROM Rental r WHERE r.user.id = :userId AND r.active = :isActive")
     List<Rental> findAllByUserIdAndActive(Long userId, Boolean isActive);
 
-    @EntityGraph(attributePaths = {"car", "user"})
-    @Query("SELECT r FROM Rental r WHERE r.returnDate <= :tomorrow AND r.actualReturnDate IS NULL")
-    List<Rental> findOverdueRentals(LocalDate tomorrow);
+    @Query("SELECT r FROM Rental r LEFT JOIN FETCH r.car LEFT JOIN FETCH"
+            + " r.user WHERE r.returnDate <= :tomorrow AND r.actualReturnDate IS NULL")
+    List<Rental> findOverdueRentals(@Param("tomorrow") LocalDate tomorrow);
 }
