d951887d9af0ca49dea969e2185473371ca6d5d4
Nazar Klimovych <rksvjdbx@gmail.com>
2024-06-03 15:21:08 +0300

    BugFix Add join fetch in findAllByUserIdAndActive to fix lazy initialization error when trying to find rentals

--- src/main/java/online/carsharing/repository/rental/RentalRepository.java
+++ src/main/java/online/carsharing/repository/rental/RentalRepository.java
@@ -6,7 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface RentalRepository extends JpaRepository<Rental, Long> {
-    @Query("SELECT r FROM Rental r WHERE r.user.id = :userId AND r.active = :isActive")
+    @Query("SELECT r FROM Rental r JOIN FETCH r.car "
+            + "WHERE r.user.id = :userId AND r.active = :isActive")
     List<Rental> findAllByUserIdAndActive(Long userId, Boolean isActive);
 
     @Query("SELECT r FROM Rental r JOIN FETCH r.user JOIN FETCH r.car WHERE r.active = :isActive")
