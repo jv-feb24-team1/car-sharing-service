b9529ced745a0eb4f421459e5841e85450f32da3
Vladyslav Vyshynskyi <73417333+azzilian@users.noreply.github.com>
2024-06-04 12:23:41 +0300

    Add Telegram scheduling implementation (#11)
    * Merge branch telegram-implementation into telegram-scheduling-implementation
    Add NotificationScheduler & NotificationSchedulerImpl - scheduler for overdue rentals
    
    Refactor:
     application.properties - BOTNAME changed to BOT_NAME, BOTTOKEN changed to BOT_TOKEN
     NotificationServiceImpl - order to createCarNotification- brand,model,type,fee
    Removed:
    NotificationSchedulerImpl - removed RentalResponseDto no longer used
    
    * Refactor Merge conflicts resolve
    
    * Refactor UserChatIdRepository now message will be be send only to one chat despite of number of users in this chat
    NotificationSchedulerImpl - logger removed
    
    * Refactor:
    UserChatId - primary key changed to new field id
    NotificationSchedulerImpl - renamed + code refactor
    RentalRepository - @Param removed
    UserChatIdRepository - method findByUserId renamed to findUserByTelegramId

--- src/main/java/online/carsharing/repository/rental/RentalRepository.java
+++ src/main/java/online/carsharing/repository/rental/RentalRepository.java
@@ -1,5 +1,6 @@
 package online.carsharing.repository.rental;
 
+import java.time.LocalDate;
 import java.util.List;
 import online.carsharing.entity.Rental;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,4 +13,8 @@ public interface RentalRepository extends JpaRepository<Rental, Long> {
 
     @Query("SELECT r FROM Rental r JOIN FETCH r.user JOIN FETCH r.car WHERE r.active = :isActive")
     List<Rental> findAllByActive(boolean isActive);
+
+    @Query("SELECT r FROM Rental r LEFT JOIN FETCH r.car LEFT JOIN FETCH"
+            + " r.user WHERE r.returnDate <= :tomorrow AND r.actualReturnDate IS NULL")
+    List<Rental> findOverdueRentals(LocalDate tomorrow);
 }
