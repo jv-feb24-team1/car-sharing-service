3c2c76025ffa4976283df6b6473b6aca95403904
Evgeniy Chernonog <chernonog.evgeniy@gmail.com>
2024-06-04 12:13:48 +0300

    Refactor -  Sql script for user test

--- src/test/resources/db/script/user_db.sql
+++ src/test/resources/db/script/user_db.sql
@@ -12,7 +12,10 @@ CREATE TABLE users (
                        email VARCHAR(255) NOT NULL UNIQUE,
                        first_name VARCHAR(50) NOT NULL,
                        last_name VARCHAR(50) NOT NULL,
-                       password VARCHAR(255) NOT NULL
+                       password VARCHAR(255) NOT NULL,
+                       is_deleted BIT NOT NULL DEFAULT FALSE
+
+
 );
 
 CREATE TABLE roles (
