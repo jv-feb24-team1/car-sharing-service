5724b1a6a4f599a6831b4be1cd5a607911fec9b8
Vladyslav Vyshynskyi <crusotdeath@gmail.com>
2024-06-04 13:15:08 +0300

    Remove BotConfig - @ComponentScan removed NotificationSchedulerImpl - Strings for constants removed, code refactor

--- src/main/java/online/carsharing/service/impl/NotificationSchedulerImpl.java
+++ src/main/java/online/carsharing/service/impl/NotificationSchedulerImpl.java
@@ -16,22 +16,12 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class NotificationSchedulerImpl implements NotificationScheduler {
+    private static final String CRON_MORNING_REMINDER =
+            "0 30 8 * * ?";
+    private static final String CRON_EVENING_REMINDER =
+            "0 30 16 * * ?";
     private static final String EXPIRED_RENTALS =
             "Overdue rental! \nRental Date: %s\nReturn Date: %s\nCar: %s\nUser: %s";
-    private static final String NO_RENTALS =
-            "No rentals overdue today!";
-    private static final String TIME_MORNING_REMINDER =
-            "0 30 8 * * ?";
-    private static final String TIME_EVENING_REMINDER =
-            "0 30 16 * * ?";
-    private static final String RENTAL_WARNING_TG_HEADER =
-            "Warning overdue rentals detected!";
-    private static final String RENTAL_WARNING_TG_TOTAL_OVERDUE =
-            "Total rentals overdue: ";
-    private static final String NO_CAR_WITH_ID =
-            "Unknown Car";
-    private static final String NO_USER_WITH_ID =
-            "Unknown User";
     private static final int DAY = 1;
 
     private final NotificationService notificationService;
@@ -41,14 +31,14 @@ public class NotificationSchedulerImpl implements NotificationScheduler {
 
     @Override
     @Async
-    @Scheduled(cron = TIME_MORNING_REMINDER)
+    @Scheduled(cron = CRON_MORNING_REMINDER)
     public void morningReminder() {
         rentalReminder();
     }
 
     @Override
     @Async
-    @Scheduled(cron = TIME_EVENING_REMINDER)
+    @Scheduled(cron = CRON_EVENING_REMINDER)
     public void eveningReminder() {
         rentalReminder();
     }
@@ -58,31 +48,15 @@ public class NotificationSchedulerImpl implements NotificationScheduler {
         List<Rental> overdueRentals = rentalRepository.findOverdueRentals(tomorrow);
 
         if (overdueRentals.isEmpty()) {
-            notificationService.sendNotification(NO_RENTALS);
+            notificationService.sendNotification("No rentals overdue today!");
         } else {
-            notificationService.sendNotification(RENTAL_WARNING_TG_HEADER);
+            notificationService.sendNotification("Warning overdue rentals detected!");
             for (Rental rental : overdueRentals) {
                 notificationService.sendNotification(createRentalOverdueMessage(rental));
             }
-            notificationService.sendNotification(RENTAL_WARNING_TG_TOTAL_OVERDUE
+            notificationService.sendNotification("Total rentals overdue: "
                     + overdueRentals.size());
         }
-    }
-
-    private String searchOverdueRentalCarModel(Rental rental) {
-        return carRepository.findById(rental
-                        .getCar()
-                        .getId())
-                .map(car -> car.getModel())
-                .orElse(NO_CAR_WITH_ID);
-    }
-
-    private String searchOverdueRentalUserEmail(Rental rental) {
-        return userRepository.findById(rental
-                        .getUser()
-                        .getId())
-                .map(user -> user.getEmail())
-                .orElse(NO_USER_WITH_ID);
     }
 
     private String createRentalOverdueMessage(Rental rental) {
@@ -94,4 +68,22 @@ public class NotificationSchedulerImpl implements NotificationScheduler {
                 searchOverdueRentalUserEmail(rental)
         );
     }
+
+    private String searchOverdueRentalCarModel(Rental rental) {
+        return carRepository.findById(rental
+                        .getCar()
+                        .getId())
+                .map(car -> car.getModel())
+                .orElse("Unknown Car");
+    }
+
+    private String searchOverdueRentalUserEmail(Rental rental) {
+        return userRepository.findById(rental
+                        .getUser()
+                        .getId())
+                .map(user -> user.getEmail())
+                .orElse("Unknown User");
+    }
+
+
 }
