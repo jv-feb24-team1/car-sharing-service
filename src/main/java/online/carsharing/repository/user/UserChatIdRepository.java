0662e89acc80b95f9ca9fcd8d701d17b48728d66
Vladyslav Vyshynskyi <crusotdeath@gmail.com>
2024-06-04 08:57:40 +0300

    Refactor UserChatIdRepository now message will be be send only to one chat despite of number of users in this chat NotificationSchedulerImp - logger removed

--- src/main/java/online/carsharing/repository/user/UserChatIdRepository.java
+++ src/main/java/online/carsharing/repository/user/UserChatIdRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface UserChatIdRepository extends JpaRepository<UserChatId, Long> {
-    @Query("SELECT uc.chatId FROM UserChatId uc")
+    @Query("SELECT DISTINCT uc.chatId FROM UserChatId uc")
     List<Long> findAllChatIds();
     
     @Query("SELECT uc FROM UserChatId uc WHERE uc.userId = :userId")
--- src/main/java/online/carsharing/service/impl/NotificationSchedulerImp.java
+++ src/main/java/online/carsharing/service/impl/NotificationSchedulerImp.java
@@ -4,14 +4,11 @@ import java.time.LocalDate;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import online.carsharing.entity.Rental;
-import online.carsharing.mapper.RentalMapper;
 import online.carsharing.repository.car.CarRepository;
 import online.carsharing.repository.rental.RentalRepository;
 import online.carsharing.repository.user.UserRepository;
 import online.carsharing.service.NotificationScheduler;
 import online.carsharing.service.NotificationService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -19,28 +16,24 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class NotificationSchedulerImp implements NotificationScheduler {
-    private static final Logger logger = LoggerFactory.getLogger(NotificationSchedulerImp.class);
     private static final String EXPIRED_RENTALS =
             "Overdue rental! \nRental Date: %s\nReturn Date: %s\nCar: %s\nUser: %s";
     private static final String NO_RENTALS =
             "No rentals overdue today!";
     private static final String TIME_MORNING_REMINDER =
-            "00 30 8 * * ?";
+            "0 30 8 * * ?";
     private static final String TIME_EVENING_REMINDER =
             "0 30 16 * * ?";
     private static final String RENTAL_WARNING_TG_HEADER =
             "Warning overdue rentals detected!";
     private static final String RENTAL_WARNING_TG_TOTAL_OVERDUE =
             "Total rentals overdue: ";
-    private static final String LOGGER_TOTAL_OVERDUE =
-            "Checked for overdue rentals. Total found: ";
     private static final int DAY = 1;
 
     private final NotificationService notificationService;
     private final RentalRepository rentalRepository;
     private final UserRepository userRepository;
     private final CarRepository carRepository;
-    private final RentalMapper rentalMapper;
 
     @Override
     @Async
