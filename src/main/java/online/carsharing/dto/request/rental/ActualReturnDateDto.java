2b920a1e3f1515f03de01726bcbb29384a42334d
Nazar <92024646+nklimovych@users.noreply.github.com>
2024-06-05 09:50:49 +0300

    Refactor Change ActualReturnDateDto validation annotation to NotNull. Add docker-compose env properties for a telegram bot and stripe payment system. (#30)

--- src/main/java/online/carsharing/dto/request/rental/ActualReturnDateDto.java
+++ src/main/java/online/carsharing/dto/request/rental/ActualReturnDateDto.java
@@ -1,11 +1,11 @@
 package online.carsharing.dto.request.rental;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import lombok.Data;
 
 @Data
 public class ActualReturnDateDto {
-    @NotBlank(message = "Actual return date is mandatory")
+    @NotNull(message = "Actual return date is mandatory")
     private LocalDate actualReturnDate;
 }
